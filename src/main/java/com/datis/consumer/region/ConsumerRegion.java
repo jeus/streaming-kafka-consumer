0ccc40fc490481d9774c06b66fd071b2296f8f70
jeus <alkhandani@gmail.com>
2016-07-25 15:18:49 +0430

    that work now get data from topic "step2" good

--- src/main/java/com/datis/consumer/region/ConsumerRegion.java
+++ src/main/java/com/datis/consumer/region/ConsumerRegion.java
@@ -45,28 +45,36 @@ public class ConsumerRegion extends Thread {
     public void run() {
         consumer.subscribe(Collections.singleton(topic));//subscribe all topics for poll
 //        while (true) {
-        System.out.println("Consumer Have to poll Consumer position:");
+        System.out.println("Consumer Have to poll Consumer position:========================");
 //            consumer.assign(partition);
 //            consumer.seekToBeginning(Collections.singleton(tp0));
 //            consumer.seek(tp0,100);
         consumer.poll(100);
-        Set<TopicPartition> setTopic = consumer.assignment();
-        System.out.println("SIIIIIZE" + setTopic.size());
-        for (TopicPartition topicPartition : setTopic) {
-            System.out.println("topic and partition:" + topicPartition.toString());
-        }
-
+//        Set<TopicPartition> setTopic = consumer.assignment();
+//        System.out.println("SIIIIIZE" + setTopic.size());
+//        for (TopicPartition topicPartition : setTopic) {
+//            System.out.println("topic and partition:" + topicPartition.toString());
+//        }
+//
         consumer.seek(tp.get(0), 1);
-        while (logOn) {
+//        consumer.seek(tp.get(0), 153820);
+        int position = 8000;
+        while (true) {
 
-            ConsumerRecords<String, Long> records = consumer.poll(10000);
-            consumer.commitSync();
+//            ConsumerRecords<String, Long>
+            ConsumerRecords<String, Long> records = consumer.poll(1000);
+//            position++;
+//            consumer.commitSync();
             System.out.println(logPosition());
+            System.out.println("---------------*******" + records.count());
+
 //            System.out.println( logPosition());
             for (ConsumerRecord<String, Long> rec : records) {
+                System.out.println("---------------------------------------");
                 if (logOn) {
 //                System.out.println("RECORD IS SAVED:(Key:" + rec.key() + ")    (Value:" + rec.value() + ")     (Partition:" + rec.partition() + ":" + rec.offset() + ")   (TimeStamp:" + rec.timestamp() + ")");
-                    System.out.println("(Key:" + rec.key() + ")    (Value:" + rec.value() + ")");
+                    System.out.println("(Key:" + rec.key() + ")    (Value:" + rec.value() + ")" + rec.offset());
+                    System.out.println("---------------------------------------");
                 }
                 partitionBenchMark(rec);
             }
@@ -90,6 +98,7 @@ public class ConsumerRegion extends Thread {
 
     private String logPosition() {
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < 1; i++) {
             try {
                 TopicPartition tp = new TopicPartition(topic, i);
@@ -117,16 +126,20 @@ public class ConsumerRegion extends Thread {
 //        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.17.0.11:2181");//172.17.0.8:2181,172.17.0.9:2181,172.17.0.10:2181");
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.17.0.13:9092");//172.17.0.8:2181,172.17.0.9:2181,172.17.0.10:2181");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "TestMikonam1");
-        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, (4000 * 10000) + "");//change this for increase and decrease packet fethe by consumer every message is 100Byte
-//        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "200");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+//        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, (4000 * 10000) + "");//change this for increase and decrease packet fethe by consumer every message is 100Byte
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "5");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_DOC, "earliest");
+//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_DOC, "latest");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongDeserializer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                //                "org.apache.kafka.common.serialization.StringDeserializer");
+                "org.apache.kafka.common.serialization.LongDeserializer");
+//        ConsumerRegion consumer1 = new ConsumerRegion("step2test1", props, true);
         ConsumerRegion consumer1 = new ConsumerRegion("step2", props, true);
         consumer1.start();
     }
-
 }
